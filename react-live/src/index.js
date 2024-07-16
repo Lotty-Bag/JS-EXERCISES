433cf6ff8ff93a2fb59e2ccb572cefbebb793278
Lotty-Bag <carlotta.bagna@gmail.com>
2024-07-16 18:13:57 +0200

    ddd

--- react-live/src/index.js
+++ react-live/src/index.js
@@ -1,11 +1,9 @@
 import _ from "lodash";
+import _ from "bootstrap";
 
 function component() {
-
     const element = document.createElement('div');
-
     element.innerHTML = _.join(['Hello','Webpack'], ' ');
-
     return element;
 }
 
